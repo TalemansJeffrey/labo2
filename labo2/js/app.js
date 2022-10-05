fa56a9bae7d725c3f5ec2301131c7f6afda3bc2f
Cheffren <jeffrey.veyron@gmail.com>
2022-10-05 16:31:50 +0200

    Initial commit

---
+++ labo2/js/app.js
@@ -0,0 +1,6 @@
+// import another class
+import Bingo from "./bingo.js";
+import "../harrystyles/nostyle.css"
+
+// kickstart the app by calling the constructor via the `new` keyword
+const bingo = new Bingo();
